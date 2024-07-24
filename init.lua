b111362f0193813a405a83279915294a2da5dfb6
Daniel Dowdle <dandoodle956@gmail.com>
2024-07-23 22:52:08 -0500

    Add markdown_inline and make trouble auto focus

--- init.lua
+++ init.lua
@@ -829,7 +829,7 @@ require('lazy').setup {
 
       ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup {
-        ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'python' },
+        ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'markdown_inline', 'vim', 'vimdoc', 'python' },
         -- Autoinstall languages that are not installed
         auto_install = true,
         highlight = { enable = true },
