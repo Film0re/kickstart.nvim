9021ed1d9d83348521867a8969d4658720c0729e
Daniel Dowdle <dandoodle956@gmail.com>
2024-08-21 22:12:30 -0500

    Add lazy-lock, unforutnately its a bit late as my vuolar is borken :)

--- init.lua
+++ init.lua
@@ -652,8 +652,8 @@ require('lazy').setup {
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        javascript = { { 'prettierd', 'prettier' } },
-        vue = { { 'prettierd', 'prettier' } },
+        javascript = { 'prettierd' },
+        vue = { 'prettierd' },
       },
     },
   },
