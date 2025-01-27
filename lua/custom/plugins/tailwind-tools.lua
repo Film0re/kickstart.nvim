0b0f311e6cd6bcbc1ac51449a456c3246a1054f0
Daniel Dowdle <dowdl021@umn.edu>
2025-01-27 14:25:34 -0600

    added ng

--- lua/custom/plugins/tailwind-tools.lua
+++ lua/custom/plugins/tailwind-tools.lua
@@ -1,8 +1,9 @@
--- tailwind-tools.lua
-vim.api.nvim_set_keymap('n', '<Leader>ts', ':TailwindSort<CR>', { noremap = true, silent = true })
-
 return {
   'luckasRanarison/tailwind-tools.nvim',
   dependencies = { 'nvim-treesitter/nvim-treesitter' },
   opts = {}, -- your configuration
+  config = function()
+    -- tailwind-tools.lua
+    vim.api.nvim_set_keymap('n', '<Leader>ts', ':TailwindSort<CR>', { noremap = true, silent = true })
+  end,
 }
