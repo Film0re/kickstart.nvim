243ebc02b96311e0fc895a6f05b32a73479d2e13
Daniel Dowdle <dandoodle956@gmail.com>
2025-02-18 20:05:34 -0600

    Add snacks nvim and begin to move towards that

--- lua/custom/plugins/ng.lua
+++ lua/custom/plugins/ng.lua
@@ -6,8 +6,8 @@ return {
     local ng = require 'ng'
 
     -- Key mappings
-    vim.keymap.set('n', '<leader>at', ng.goto_template_for_component, opts)
-    vim.keymap.set('n', '<leader>ac', ng.goto_component_with_template_file, opts)
+    vim.keymap.set('n', '<leader>gat', ng.goto_template_for_component, opts)
+    vim.keymap.set('n', '<leader>gac', ng.goto_component_with_template_file, opts)
     vim.keymap.set('n', '<leader>aT', ng.get_template_tcb, opts)
   end,
 }
