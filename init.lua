1306b7f4cb1496cc8bb137fdef812405374eb722
Daniel Dowdle <dowdl021@umn.edu>
2025-02-19 14:57:54 -0600

    Adds mypy, basedpyright to mason ensure installed

--- init.lua
+++ init.lua
@@ -567,9 +567,12 @@ require('lazy').setup {
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        ts_ls = {},
+        -- ts_ls = {},
         tailwindcss = {},
         -- pylyzer = {},
+        -- Python shtuff
+        basedpyright = {},
+        mypy = {},
         lua_ls = {
           -- cmd = {...},
           -- filetypes { ...},
